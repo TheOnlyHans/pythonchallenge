e64b4d7bf957c10b78ae30a3eacd9aebf9920239
Justin <32331326+Jburd333@users.noreply.github.com>
2017-11-14 21:37:14 -0500

    py chal 4 complete
    complete

--- py_chal_4.py
+++ py_chal_4.py
@@ -1,5 +1,33 @@
 import urllib.request
-pc = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345')
-x = pc.read()[-1:-6:-1]
+'''
+y= 12345
+new = ''
+pc = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(y))
+x = pc.readline()
+y = str(x)
+print(y[1:5])
+
+
 x = x[::-1]
-print(x)
+for char in x:
+    if str(char).isdigit():
+        new += str(x)
+x = new
+print(x[1])
+'''
+
+
+
+count = 1
+nothing = 12345
+
+while count < 401:
+    pc = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+str(nothing))
+    x = str(pc.read())
+    nothing = ''
+    for num in x:
+        if num.isdigit():
+            nothing += str(num)
+    print(nothing)
+    count +=1
+    
